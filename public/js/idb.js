e882433e30df82343136ca22140efe419136179d
Matt Foster <fmatthew40@gmail.com>
2022-03-09 17:26:38 -0700

    add offline functionality

---
+++ public/js/idb.js
@@ -0,0 +1,25 @@
+let db;
+
+const request = indexedDB.open('budget_binder', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_budget', { autoIncrement: true });
+  };
+request.onsuccess = function(event) {
+    db = event.target.result;
+      if (navigator.onLine) {
+      // we haven't created this yet, but we will soon, so let's comment it out for now
+      // uploadPizza();
+    }
+  };
+  
+  request.onerror = function(event) {
+    console.log(event.target.errorCode);
+  };
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_budget');
+    budgetObjectStore.add(record);
+  }
